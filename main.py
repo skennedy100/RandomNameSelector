627a034dc19c31ebd83b17e243aa1e2038e8977a
skennedy100 <34197856+skennedy100@users.noreply.github.com>
2022-01-25 00:24:56 +0000

    Initial Inmplementation
    Added basic pop-up with names and some sample URLs (to be replaced with Jira URLs later) and a simple sound

---
+++ main.py
@@ -0,0 +1,57 @@
+from pickle import TRUE
+import random
+from token import EXACT_TOKEN_TYPES
+import easygui as gui
+import webbrowser
+from playsound import playsound
+
+class RandomNameSelector:
+    def __init__(self):
+        self.namesAndURLs=[]
+
+    def LoadNames(self, filename):
+        status = True
+        try:
+            file = open(filename, 'r')
+
+            for line in file:
+                self.namesAndURLs.append(line.strip())
+
+            file.close()
+        except FileNotFoundError:
+            print('Invalid file path:', filename)
+            status = False
+
+        # print(self.namesAndURLs)
+        return status
+
+    def SelectRandomNames(self):
+        length = len(self.namesAndURLs)
+        i = 0
+        while i < length:
+            try:
+                playsound('sounds/DrumRoll.wav')
+            except Exception:
+                pass
+            
+            nameAndURL = random.choice(self.namesAndURLs)
+            nameAndUrlList = nameAndURL.split(",")
+            name=nameAndUrlList[0]
+            gui.msgbox(name, title="Victim",ok_button="Press here to being subjugation...")
+            
+            try:
+                url=nameAndUrlList[1]
+                if len(url) > 0:
+                    webbrowser.open(url.strip())
+            except Exception:
+                pass
+            
+            self.namesAndURLs.remove(nameAndURL)
+            i = i + 1
+            gui.msgbox("If the collective are Satisfied with your tribute.", title="Proceeding...",ok_button="Press here to subjugate next victim.")
+            # input('Press any key to continue...')
+
+if __name__ == "__main__":
+    rns = RandomNameSelector()
+    if (rns.LoadNames('names.txt')):
+        rns.SelectRandomNames()
